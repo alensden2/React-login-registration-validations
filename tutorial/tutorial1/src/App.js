5d6f2686de590e99cc78124144c02541b7c3902c
alensden2 <alensanjohn1999@gmail.com>
2023-05-24 17:41:54 -0300

    initial commit --structure_set_up --Created_react_App

---
+++ tutorial/tutorial1/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi, This is Alen's first react app, deployed to netlify!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
