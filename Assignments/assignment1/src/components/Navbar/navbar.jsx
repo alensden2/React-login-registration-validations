0f9ffd0847fe963d0a4bbbd1c699e4b66e076ba6
alensden2 <alensanjohn1999@gmail.com>
2023-05-26 16:23:13 -0300

    side bar set

--- Assignments/assignment1/src/components/Navbar/navbar.jsx
+++ Assignments/assignment1/src/components/Navbar/navbar.jsx
@@ -1,33 +1,144 @@
-import React from 'react';
-import { Box, Typography, AppBar, Toolbar } from '@mui/material';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import MenuIcon from "@mui/icons-material/Menu";
+import { AppBar, Box, Drawer, Toolbar, Typography } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import { styled, useTheme } from "@mui/material/styles";
+import React, { useState } from "react";
 
+const drawerWidth = 240;
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
+
+const AppBarStyled = styled(AppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
 const Navbar = () => {
+  const [isSideOpen, setIsSideOpen] = useState(false);
+  const theme = useTheme();
+  const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+    ({ theme, open }) => ({
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: `-${drawerWidth}px`,
+      ...(open && {
+        transition: theme.transitions.create("margin", {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+      }),
+    })
+  );
+
+  const handleSideClick = () => {
+    setIsSideOpen(!isSideOpen);
+  };
+
+  const handleDrawerClose = () => {
+    setIsSideOpen(false);
+  };
+
   return (
-    <Box>
-      <AppBar sx={{
-        backgroundColor: 'white',
-    }}>
+    <Box sx={{ display: "flex" }}>
+      <AppBarStyled
+        position="fixed"
+        open={isSideOpen}
+        sx={{ backgroundColor: "white" }}
+      >
         <Toolbar>
           <IconButton
-            size="large"
-            edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{ mr: 2 }}
+            onClick={handleSideClick}
+            edge="start"
+            sx={{ mr: 2, ...(isSideOpen && { display: "none" }) }}
           >
-            <MenuIcon sx={{
-                color: "black"
-            }}/>
+            <MenuIcon
+              sx={{
+                color: "black",
+              }}
+            />
           </IconButton>
-          <Typography sx={{
-            fontFamily: 'Lobster', fontSize: '1.5rem', color: 'black'
-          }}>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{
+              fontFamily: "Lobster",
+              fontSize: "1.5rem",
+              color: "black",
+            }}
+          >
             VogueManic
           </Typography>
         </Toolbar>
-      </AppBar>
+      </AppBarStyled>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={isSideOpen}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+      </Drawer>
     </Box>
   );
 };
@@ -35,4 +146,5 @@ const Navbar = () => {
 export default Navbar;
 
 // referene navbar - https://mui.com/material-ui/react-app-bar/
+// reference side bar - https://mui.com/material-ui/react-drawer/
 // font reference -<link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
