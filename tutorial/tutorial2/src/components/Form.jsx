3c1c204f7cbdc93de6c13c2c020a520a0f5be4e6
alensden2 <alensanjohn1999@gmail.com>
2023-06-07 18:28:31 -0300

    Validations for first and last name added

--- tutorial/tutorial2/src/components/Form.jsx
+++ tutorial/tutorial2/src/components/Form.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 
 function Form({ isLogin }) {
+    const [firstName, setFirstName] = useState('');
+    const [firstNameError, setFirstNameError] = useState('');
+
+    const [lastName, setLastName] = useState('');
+    const [lastNameError, setLastNameError] = useState('');
+
+    const validateFirstName = (name) => {
+        const regex = /^[A-Za-z]+$/;
+        return regex.test(name);
+    };
+
+    useEffect(() => {
+        if (firstName.trim() !== '' && !validateFirstName(firstName)) {
+            setFirstNameError('First name can only contain alphabets')
+        } else {
+            setFirstNameError('');
+        }
+    }, [firstName]);
+
+    useEffect(() => {
+        if (lastName.trim() !=='' && !validateFirstName(lastName)){
+            setLastNameError('Last name can only contain alphabets')
+        } else {
+            setLastNameError('');
+        }
+    }, [lastName])
+
+
+
     return (
         <Box sx={{
             width: '300px',
@@ -14,12 +43,24 @@ function Form({ isLogin }) {
             textAlign: 'center',
             fontWeight: 'bold'
         }}>
-            <h3 style={{ marginBottom: '5px' }}>{isLogin ? 'Login Form' : 'Registration Form'}</h3>
+            <h3 style={{ marginBottom: '5px', marginTop: '5px' }}>{isLogin ? 'Login Form' : 'Registration Form'}</h3>
             <form>
                 {!isLogin && (
                     <Box sx={{ display: 'flex', gap: '10px', marginBottom: '2px' }}>
-                        <TextField label="First Name" variant="outlined" fullWidth margin="normal" />
-                        <TextField label="Last Name" variant="outlined" fullWidth margin="normal" />
+                        <TextField label="First Name" variant="outlined" fullWidth margin="normal" error={Boolean(firstNameError)} value={firstName}
+                        onChange={(e) => {setFirstName(e.target.value)}}
+                        helperText={firstNameError}
+                        onBlur={() => {if (firstName.trim() === '') {setFirstNameError('First name cannot be empty');}}}
+                        />
+                        <TextField label="Last Name" variant="outlined" fullWidth margin="normal" 
+                        value={lastName} 
+                        helperText={lastNameError} error={Boolean(lastNameError)}
+                        onBlur={() => {
+                            if (lastName.trim() === '') {
+                              setLastNameError('Last name cannot be empty');
+                            }
+                          }}
+                          onChange={(e) => setLastName(e.target.value)}/>
                     </Box>
                 )}
 
@@ -38,10 +79,9 @@ function Form({ isLogin }) {
                         fullWidth
                         margin="normal"
                         type="password"
-                        style={{ marginBottom: '20px' }}
                     />
                 )}
-                <Button variant="contained" color="secondary" type="submit" sx={{ fontWeight: 'bold' }}>
+                <Button style={{ marginTop: '17px', marginBottom: '7px' }} variant="contained" color="secondary" type="submit" sx={{ fontWeight: 'bold' }}>
                     {isLogin ? 'Login' : 'Register'}
                 </Button>
             </form>
@@ -50,3 +90,6 @@ function Form({ isLogin }) {
 }
 
 export default Form;
+
+/** reference for regex - https://regexr.com/ */
+/** refernce for form validation https://stackoverflow.com/questions/41296668/how-do-i-add-validation-to-the-form-in-my-react-component */
